965f4d9ce02debe2d8a65de06ec3ecfc4a4e7d9f
Roman Meinl <roman-meinl@ukr.net>
2025-12-10 14:26:07 -0200

    10.12 none blocking while

---
+++ 02- bloking-event-loop/noneBlockingWhile.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const dns = require("dns");
+
+
+let isRunning = true;
+
+setTimeout(() => {
+    isRunning = false
+}, 10);
+
+process.nextTick(() => {
+    console.log("nextTick",performance.now().toFixed(2));
+    
+})
+
+function setImmediatePromise(){
+    return new Promise((resolve , reject) => {
+      setImmediate(() => resolve())
+    })
+}
+
+
+async function whileLoop() {
+    while(isRunning){
+    console.log("While loop is running ...",performance.now().toFixed(2));
+    await setImmediatePromise()
+}
+}
+
+whileLoop().then(() => console.log("while loop ended",performance.now().toFixed(2))
+)
+
---
+++ 02- bloking-event-loop/while.js
@@ -0,0 +1,17 @@
+
+let isRunning = true;
+
+setTimeout(() => {
+    isRunning = false
+}, 0);
+
+process.nextTick(() => {
+    console.log("nextTick");
+    
+})
+
+
+while(isRunning){
+    console.log("While loop is running ...",performance.now().toFixed(2));
+    
+}
